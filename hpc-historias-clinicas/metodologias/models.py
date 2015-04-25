04c69c9836a9f66ffeeed6798ca2926422c87d69
btenaglia <brunomartintenaglia@gmail.com>
2015-04-25 15:28:20 -0300

    Agrego tipo de metodologias

--- hpc-historias-clinicas/metodologias/models.py
+++ hpc-historias-clinicas/metodologias/models.py
@@ -3,5 +3,14 @@ from django.db import models
 from ..core.models import TimeStampedModel
 
 
+class TipoMetodologias(TimeStampedModel):
+    nombre = models.CharField(max_length=150, blank=False, null=False, verbose_name=u'Metodología')
+
+    def __unicode__(self):
+        return self.nombre
+
+
 class Metodologias(TimeStampedModel):
+    tipo_metodologia = models.ForeignKey(TipoMetodologias, blank=True, null=True,
+                                         verbose_name=u'Metodología')
     metodologia = models.TextField(blank=True, null=True, verbose_name=u'Descripción')
