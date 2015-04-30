779b665fed560a5e484c10a4949c15a88e542e51
btenaglia <alumno@curso>
2015-04-30 18:44:10 -0300

    Fojas quirurgicas WIP

--- hpc-historias-clinicas/fojas_quirurgicas/urls.py
+++ hpc-historias-clinicas/fojas_quirurgicas/urls.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
-from .views import FojasQuirurgicasListView
+from .views import ( FojasQuirurgicasListView, 
+	FojasQuirurgicasCreateView, 
+	FojasQuirurgicasUpdateView, 
+	FojasQuirurgicasDeleteView)
 
 urlpatterns = patterns('',
 
     url(r'^/(?P<historia>\d+)$', FojasQuirurgicasListView.as_view(), name='list'),
+    url(r'^/create/(?P<historia>\d+)$', FojasQuirurgicasCreateView.as_view(), name='create'),
+    url(r'^/update/(?P<pk>\d+)/(?P<historia>\d+)$', FojasQuirurgicasUpdateView.as_view(), name='update'),
+    url(r'^/delete/(?P<pk>\d+)/(?P<historia>\d+)$', FojasQuirurgicasDeleteView.as_view(), name='delete'),
 
 )
 
--- hpc-historias-clinicas/fojas_quirurgicas/views.py
+++ hpc-historias-clinicas/fojas_quirurgicas/views.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
 from django.contrib import messages
 from braces.views import LoginRequiredMixin
 
@@ -10,8 +12,8 @@ class FojasQuirurgicasMixin(object):
     def success_msg(self):
         return NotImplemented
 
-    def descarga_msg(self, ic_id):
-        return " Click en el siguiente link para Descargar e Imprimir</a>"
+    def descarga_msg(self, foja_id):
+        return " Click en el siguiente link para <a href='/reportes/foja/quirurgica/%s'>Descargar e Imprimir</a>" % str(foja_id)
 
     def get_context_data(self, **kwargs):
         """Es necesario traer los datos de la historia clinica"""
@@ -28,5 +30,54 @@ class FojasQuirurgicasListView(LoginRequiredMixin, FojasQuirurgicasMixin, ListVi
     """Listado de fojas quirurgicas de las historias"""
     def get_queryset(self):
         qs = FojasQuirurgicas.objects.filter(historia=self.kwargs['historia'])
-
+        # -- TODO - Filtros
         return qs
+
+
+class FojasQuirurgicasCreateView(LoginRequiredMixin, FojasQuirurgicasMixin, CreateView):
+    """Nueva foja quirurgica"""
+    model = FojasQuirurgicas
+    fields = ['cirujano', 'ayudante_1', 'ayudante_2', 'ayudante_3', 'fecha', 'hora_comienzo', 'hora_fin']
+    success_msg = 'La foja quirurgica se agregó con éxito.'
+
+    def post(self, request, *args, **kwargs):
+        # -- hay que ndicarle el Id de la historia
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        self.object = None
+
+        form.instance.historia_id = self.kwargs['historia']
+        if form.is_valid():
+            form.save()
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(**{'form': form})
+
+    def get_success_url(self):
+        # necesitamos el ultimo id para
+        # armar el link de descarga
+        pk = FojasQuirurgicas.objects.latest('id').id
+        if pk:
+            self.success_msg += self.descarga_msg(pk)
+
+        return super(FojasQuirurgicasCreateView, self).get_success_url()
+
+
+class FojasQuirurgicasUpdateView(LoginRequiredMixin, FojasQuirurgicasMixin, UpdateView):
+    """ Edicion de una foja quirurgica """
+    model = FojasQuirurgicas
+    fields = ['cirujano', 'ayudante_1', 'ayudante_2', 'ayudante_3', 'fecha', 'hora_comienzo', 'hora_fin']
+    success_msg = 'La foja quirurgica se editó con éxito.'  
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        if pk:
+            self.success_msg += self.descarga_msg(pk)
+
+        return super(FojasQuirurgicasUpdateView, self).get_success_url() 
+
+
+class FojasQuirurgicasDeleteView(LoginRequiredMixin, FojasQuirurgicasMixin, DeleteView):
+    """ Eliminar una foja quirurgica """
+    model = FojasQuirurgicas
+    success_msg = 'La foja quirurgica se eliminó con éxito.'          
