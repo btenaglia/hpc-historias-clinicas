592beddbf634b5e8e9c4c7db63f783b3d4b97001
btenaglia <alumno@curso>
2015-05-06 20:31:17 -0300

    Ocultar comentario de ubicaciones.

--- hpc-historias-clinicas/staticfiles/js/project.js
+++ hpc-historias-clinicas/staticfiles/js/project.js
@@ -25,6 +25,22 @@ $(document).ready(function() {
         html: true
     });
 
+    /* -------------------------------------------------
+        Ubicaciones - mostrar/ocultar el campo Otros
+    --------------------------------------------------- */
+    if ($('#id_cama ').val()!='Otros'){
+        $('#ubicaciones_form ').children("#div_id_comentario").hide();
+    }
+    $('#id_cama ').on('change', mostrar_comentario_ubicacion);
+    function mostrar_comentario_ubicacion(){
+        if ($('#id_cama ').val() == 'Otros'){
+            $('#ubicaciones_form ').children("#div_id_comentario").show();
+        } else {
+            $('#ubicaciones_form [name="comentario"]').val("");
+            $('#ubicaciones_form').children("#div_id_comentario").hide();
+        }
+    }
+
      /* --------------------------------------------------------------------------------------------
      -- Rellenar el text area de enfermedad trayendo la descripcion de -----------------------------
      -- un motivo deconsulta           -------------------------------------------------------------
