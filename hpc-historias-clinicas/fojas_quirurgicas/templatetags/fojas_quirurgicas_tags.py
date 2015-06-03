2030c2ec5328919f24b1874eff0128af797a5689
btenaglia <brunomartintenaglia@gmail.com>
2015-06-03 01:19:33 -0300

    Revert "Reporte de internados"
    This reverts commit 172e10face38fa1ffb9f16ae56ea3648d02d3036.

--- hpc-historias-clinicas/fojas_quirurgicas/templatetags/fojas_quirurgicas_tags.py
+++ hpc-historias-clinicas/fojas_quirurgicas/templatetags/fojas_quirurgicas_tags.py
@@ -9,11 +9,3 @@ def total_fojas_quirurgicas(historia_id):
     Devuelve el total de fojas quirurgicas para una historia clinica
     """
     return FojasQuirurgicas.objects.filter(historia=historia_id).count()
-
-
-@register.assignment_tag
-def obtener_fojas_quirurgicas(historia_id):
-    """
-    Obtener ls fojas quirurgicas de una determinada historia clinica
-    """
-    return FojasQuirurgicas.objects.filter(historia=historia_id).all()
