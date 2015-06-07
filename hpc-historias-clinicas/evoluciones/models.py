5e22d4b48b6ac66e73e93dd43c93cad6a3470d8c
btenaglia <brunomartintenaglia@gmail.com>
2015-06-07 13:58:01 -0300

    Agrego valor por default en Evoluciones.

--- hpc-historias-clinicas/evoluciones/models.py
+++ hpc-historias-clinicas/evoluciones/models.py
@@ -1,11 +1,19 @@
+# -*- coding: utf-8 -*-
 from datetime import datetime
 from django.db import models
 
 from ..core.models import TimeStampedModel
 from ..historias.models import Historias
 
+DESCRIPCION_DEFAULT = 'Paciente de .... años de edad que cursa primer dia de ' \
+                      'internación por cuadro de colecistitis aguda. Al examen ' \
+                      'físico se encuentra lúcido, vigil y orientado globalmente. ' \
+                      'Normotenso, afebril y hemodinamicamente estable con buena ' \
+                      'mecánica respiratoria. Abdomen blando, depresible e indoloro. ' \
+                      'RHA positivos. Antibioticoterapia con Ampicilina y Gentamicina.'
+
 
 class Evoluciones(TimeStampedModel):
     historia = models.ForeignKey(Historias, blank=False, null=False)
     fecha = models.DateField(blank=False, null=False, default=datetime.now())
-    descripcion = models.TextField(blank=False, null=False)
+    descripcion = models.TextField(blank=False, null=False, default=DESCRIPCION_DEFAULT)
